fb7ca41e2b4f423daa1fba0cea2fac15be0f06f3
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-07-20 00:49:59 +0330

    managing form with react-hook-form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,31 @@
-import {FormEvent} from "react";
+import {FieldValues, useForm} from "react-hook-form";
 const Form = () => {
-    const handleSubmit = (event: FormEvent) => {
-        event.preventDefault();
-        console.log("submited")
+    const {register, handleSubmit} = useForm();
+
+    const onSubmit = (data: FieldValues) => {
+        console.log(data)
     }
+
+
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input id="name" type="text" className="form-control" />
+                <input
+                    {...register("name")}
+                    id="name"
+                    type="text"
+                    className="form-control"
+                />
             </div>
             <div className="mb-3">
                 <label htmlFor="age" className="form-label">Age</label>
-                <input id="age" type="number" className="form-control" />
+                <input
+                    {...register("age")}
+                    id="age"
+                    type="number"
+                    className="form-control"
+                />
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
